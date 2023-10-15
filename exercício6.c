1d132bebc4dd69361be6623d780943142c513350
HenriqueMedeirosCosta <132111448+HenriqueMedeirosCosta@users.noreply.github.com>
2023-10-14 22:23:51 -0300

    Create exercício6.c

---
+++ exercício6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+ main() {
+
+  /*int num[10],maiorValor=0,menorValor=0;
+
+  for(int i=0;i<10;i++){
+      printf("\nDigite um numero:\n");
+      scanf("%d",&num[i]);
+
+      if(num[i] > maiorValor){
+        maiorValor=num[i];
+        
+      }
+
+      if(num[i]<menorValor|| num[i]==num[0]){
+        menorValor=num[i];
+      }
+  }
+
+   printf("\nMaior elemento: %d",maiorValor);
+   printf("\nMenor elemento: %d",menorValor);*/
+}
