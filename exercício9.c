8f8774229497fb54e5e7fa327dbd9f036c2b1680
HenriqueMedeirosCosta <132111448+HenriqueMedeirosCosta@users.noreply.github.com>
2023-10-14 22:27:18 -0300

    Create exercício9.c

---
+++ exercício9.c
@@ -0,0 +1,25 @@
+#include<Stdio.h>
+
+main(){
+int num[6];
+
+  for(int i=0;i<6 ;i++){
+      
+     printf("\nDigite um numero par:\n");
+     scanf("%d",&num[i]);
+
+      if(num[i]%2 !=0){
+        printf("\nO numero digitado, não é um numero par.\n");
+         i--;
+      }
+
+  }
+
+   for(int i=5;i<6 ;i--){
+     printf("\n%d",num[i]);
+
+     if(num[i]==num[0]){
+       break;
+     }
+   }
+}
